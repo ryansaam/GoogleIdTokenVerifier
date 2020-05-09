bab785ada9ee25f22def4668b66de6ac22868cf5
Ryan Sam <ryansam.inquiries@gmail.com>
2020-05-09 16:46:16 -0500

    removed tabs

--- googletokenidverifire.go
+++ googletokenidverifire.go
@@ -1,53 +1,53 @@
 package GoogleIdTokenVerifier
 
 import (
-	"bytes"
-	"crypto"
-	"crypto/rsa"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/binary"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"math/big"
-	"net/http"
-	"strings"
-	"time"
+  "bytes"
+  "crypto"
+  "crypto/rsa"
+  "crypto/sha256"
+  "encoding/base64"
+  "encoding/binary"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "math/big"
+  "net/http"
+  "strings"
+  "time"
 )
 
 // Certs is
 type Certs struct {
-	Keys []keys `json:"keys"`
+  Keys []keys `json:"keys"`
 }
 
 type keys struct {
-	Kty string `json:"kty"`
-	Alg string `json:"alg"`
-	Use string `json:"use"`
-	Kid string `json:"Kid"`
-	N   string `json:"n"`
-	E   string `json:"e"`
+  Kty string `json:"kty"`
+  Alg string `json:"alg"`
+  Use string `json:"use"`
+  Kid string `json:"Kid"`
+  N   string `json:"n"`
+  E   string `json:"e"`
 }
 
 // TokenInfo is
 type TokenInfo struct {
-	Sub           string `json:"sub"`
-	Email         string `json:"email"`
-	AtHash        string `json:"at_hash"`
-	Aud           string `json:"aud"`
-	EmailVerified bool   `json:"email_verified"`
-	Name          string `json:"name"`
-	GivenName     string `json:"given_name"`
-	FamilyName    string `json:"family_name"`
-	Picture       string `json:"picture"`
-	Local         string `json:"locale"`
-	Iss           string `json:"iss"`
-	Azp           string `json:"azp"`
-	Iat           int64  `json:"iat"`
-	Exp           int64  `json:"exp"`
+  Sub           string `json:"sub"`
+  Email         string `json:"email"`
+  AtHash        string `json:"at_hash"`
+  Aud           string `json:"aud"`
+  EmailVerified bool   `json:"email_verified"`
+  Name          string `json:"name"`
+  GivenName     string `json:"given_name"`
+  FamilyName    string `json:"family_name"`
+  Picture       string `json:"picture"`
+  Local         string `json:"locale"`
+  Iss           string `json:"iss"`
+  Azp           string `json:"azp"`
+  Iat           int64  `json:"iat"`
+  Exp           int64  `json:"exp"`
 }
 
 // https://developers.google.com/identity/sign-in/web/backend-auth
@@ -55,140 +55,129 @@ type TokenInfo struct {
 
 // Verify is
 func Verify(authToken string, aud string) *TokenInfo {
-	return VerifyGoogleIDToken(authToken, GetCerts(GetCertsFromURL()), aud)
+  return VerifyGoogleIDToken(authToken, GetCerts(GetCertsFromURL()), aud)
 }
 
 // VerifyGoogleIDToken is
 func VerifyGoogleIDToken(authToken string, certs *Certs, aud string) *TokenInfo {
-	header, payload, signature, messageToSign := divideAuthToken(authToken)
+  header, payload, signature, messageToSign := divideAuthToken(authToken)
 
-	tokenInfo := getTokenInfo(payload)
-	var nilTokenInfo *TokenInfo
-	//fmt.Println(tokenInfo)
-	if aud != tokenInfo.Aud {
-		err := errors.New("Token is not valid, Audience from token and certificate don't match")
-		fmt.Printf("Error verifying key %s\n", err.Error())
-		return nilTokenInfo
-	}
-	if (tokenInfo.Iss != "accounts.google.com") && (tokenInfo.Iss != "https://accounts.google.com") {
-		err := errors.New("Token is not valid, ISS from token and certificate don't match")
-		fmt.Printf("Error verifying key %s\n", err.Error())
-		return nilTokenInfo
-	}
-	if !checkTime(tokenInfo) {
-		err := errors.New("Token is not valid, Token is expired.")
-		fmt.Printf("Error verifying key %s\n", err.Error())
-		return nilTokenInfo
-	}
+  tokenInfo := getTokenInfo(payload)
+  var nilTokenInfo *TokenInfo
+  //fmt.Println(tokenInfo)
+  if aud != tokenInfo.Aud {
+    err := errors.New("Token is not valid, Audience from token and certificate don't match")
+    fmt.Printf("Error verifying key %s\n", err.Error())
+    return nilTokenInfo
+  }
+  if (tokenInfo.Iss != "accounts.google.com") && (tokenInfo.Iss != "https://accounts.google.com") {
+    err := errors.New("Token is not valid, ISS from token and certificate don't match")
+    fmt.Printf("Error verifying key %s\n", err.Error())
+    return nilTokenInfo
+  }
+  if !checkTime(tokenInfo) {
+    err := errors.New("Token is not valid, Token is expired.")
+    fmt.Printf("Error verifying key %s\n", err.Error())
+    return nilTokenInfo
+  }
 
-	key, err := choiceKeyByKeyID(certs.Keys, getAuthTokenKeyID(header))
-	if err != nil {
-		fmt.Printf("Error verifying key %s\n", err.Error())
-		return nilTokenInfo
-	}
-	pKey := rsa.PublicKey{N: byteToInt(urlsafeB64decode(key.N)), E: btrToInt(byteToBtr(urlsafeB64decode(key.E)))}
-	err = rsa.VerifyPKCS1v15(&pKey, crypto.SHA256, messageToSign, signature)
-	if err != nil {
-		fmt.Printf("Error verifying key %s\n", err.Error())
-		return nilTokenInfo
-	}
-	return tokenInfo
+  key, err := choiceKeyByKeyID(certs.Keys, getAuthTokenKeyID(header))
+  if err != nil {
+    fmt.Printf("Error verifying key %s\n", err.Error())
+    return nilTokenInfo
+  }
+  pKey := rsa.PublicKey{N: byteToInt(urlsafeB64decode(key.N)), E: btrToInt(byteToBtr(urlsafeB64decode(key.E)))}
+  err = rsa.VerifyPKCS1v15(&pKey, crypto.SHA256, messageToSign, signature)
+  if err != nil {
+    fmt.Printf("Error verifying key %s\n", err.Error())
+    return nilTokenInfo
+  }
+  return tokenInfo
 }
 
 func getTokenInfo(payloadByte []byte) *TokenInfo {
-	var tokenInfo *TokenInfo
-	json.Unmarshal(payloadByte, &tokenInfo)
-	return tokenInfo
+  var tokenInfo *TokenInfo
+  json.Unmarshal(payloadByte, &tokenInfo)
+  return tokenInfo
 }
 
 func checkTime(tokenInfo *TokenInfo) bool {
-	if (time.Now().Unix() < tokenInfo.Iat) || (time.Now().Unix() > tokenInfo.Exp) {
-		return false
+  if (time.Now().Unix() < tokenInfo.Iat) || (time.Now().Unix() > tokenInfo.Exp) {
+    return false
 	}
-	return true
+  return true
 }
 
 //GetCertsFromURL is
 func GetCertsFromURL() []byte {
-	response, _ := http.Get("https://www.googleapis.com/oauth2/v3/certs")
-	certs, _ := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	return certs
+  response, _ := http.Get("https://www.googleapis.com/oauth2/v3/certs")
+  certs, _ := ioutil.ReadAll(response.Body)
+  response.Body.Close()
+  return certs
 }
 
 //GetCerts is
 func GetCerts(certsByte []byte) *Certs {
-	var certs *Certs
-	json.Unmarshal(certsByte, &certs)
-	return certs
+  var certs *Certs
+  json.Unmarshal(certsByte, &certs)
+  return certs
 }
 
 func urlsafeB64decode(str string) []byte {
-	if m := len(str) % 4; m != 0 {
-		str += strings.Repeat("=", 4-m)
+  if m := len(str) % 4; m != 0 {
+    str += strings.Repeat("=", 4-m)
 	}
-	bt, _ := base64.URLEncoding.DecodeString(str)
-	return bt
+  bt, _ := base64.URLEncoding.DecodeString(str)
+  return bt
 }
 
 func choiceKeyByKeyID(keyArray []keys, tokenKid string) (keys, error) {
-	//if len(keyArray) == 3 {
-	//	if keyArray[0].Kid == tokenKid {
-	//		return keyArray[0], nil
-	//	}
-	//	if keyArray[1].Kid == tokenKid {
-	//		return keyArray[1], nil
-	//	}
-  //  if keyArray[2].Kid == tokenKid {
-  //    return keyArray[2], nil
-  //  }
-	//}
   for i, key := range keyArray {
     if key.Kid == tokenKid {
       return keyArray[i], nil
     }
   }
-	err := errors.New("Token is not valid, kid from token and certificate don't match")
-	var nilKeys keys
-	return nilKeys, err
+  err := errors.New("Token is not valid, kid from token and certificate don't match")
+  var nilKeys keys
+  return nilKeys, err
 }
 
 func getAuthTokenKeyID(jwtHeaderByte []byte) string {
-	var tokenKeys keys
-	json.Unmarshal(jwtHeaderByte, &tokenKeys)
-	return tokenKeys.Kid
+  var tokenKeys keys
+  json.Unmarshal(jwtHeaderByte, &tokenKeys)
+  return tokenKeys.Kid
 }
 
 func divideAuthToken(str string) ([]byte, []byte, []byte, []byte) {
-	args := strings.Split(str, ".")
-	return urlsafeB64decode(args[0]), urlsafeB64decode(args[1]), urlsafeB64decode(args[2]), calcSum(args[0] + "." + args[1])
+  args := strings.Split(str, ".")
+  return urlsafeB64decode(args[0]), urlsafeB64decode(args[1]), urlsafeB64decode(args[2]), calcSum(args[0] + "." + args[1])
 }
 
 func byteToBtr(bt0 []byte) *bytes.Reader {
-	var bt1 []byte
-	if len(bt0) < 8 {
-		bt1 = make([]byte, 8-len(bt0), 8)
-		bt1 = append(bt1, bt0...)
-	} else {
-		bt1 = bt0
-	}
-	return bytes.NewReader(bt1)
+  var bt1 []byte
+  if len(bt0) < 8 {
+    bt1 = make([]byte, 8-len(bt0), 8)
+    bt1 = append(bt1, bt0...)
+  } else {
+    bt1 = bt0
+  }
+  return bytes.NewReader(bt1)
 }
 
 func calcSum(str string) []byte {
-	a := sha256.New()
-	a.Write([]byte(str))
-	return a.Sum(nil)
+  a := sha256.New()
+  a.Write([]byte(str))
+  return a.Sum(nil)
 }
 
 func btrToInt(a io.Reader) int {
-	var e uint64
-	binary.Read(a, binary.BigEndian, &e)
-	return int(e)
+  var e uint64
+  binary.Read(a, binary.BigEndian, &e)
+  return int(e)
 }
 
 func byteToInt(bt []byte) *big.Int {
-	a := big.NewInt(0)
-	a.SetBytes(bt)
-	return a
+  a := big.NewInt(0)
+  a.SetBytes(bt)
+  return a
 }
